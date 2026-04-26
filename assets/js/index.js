c6f17ca9b5a6ad5dfd7114bd86f1d570e2091122
Michał Olejniczak <blind.x6@gmail.com>
2026-04-26 13:42:50 +0200

    solid base

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,10 +4,32 @@
 import "../css/index.css";
 
 // Import JS
-import menuOpen from "./menuOpen";
+// import menuOpen from "./menuOpen";
 import infiniteScroll from "./infiniteScroll";
 
 
 // Call the menu and infinite scroll functions
-menuOpen();
+// menuOpen();
 infiniteScroll();
+
+
+function revealOnScroll() {
+    const els = document.querySelectorAll('.reveal');
+    if (!els.length) return;
+ 
+    const io = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((e) => {
+                if (e.isIntersecting) {
+                    e.target.classList.add('in');
+                    io.unobserve(e.target);
+                }
+            });
+        },
+        { threshold: 0.1, rootMargin: '0px 0px -60px 0px' }
+    );
+ 
+    els.forEach((el) => io.observe(el));
+}
+ 
+revealOnScroll();
